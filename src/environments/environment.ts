3795f1b2bd9edf801f7cd023084404c0a0bc5e8b
Rokolya József <h268805@stud.u-szeged.hu>
2025-04-09 21:15:41 +0200

    Első mérföldkő frissites

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyCF5V4ibgv3Os2ReUF32ReaOCmarpZ_VT0",
+      authDomain: "gobusticket-f83e1.firebaseapp.com",
+      projectId: "gobusticket-f83e1",
+      storageBucket: "gobusticket-f83e1.appspot.com",
+      messagingSenderId: "883592815570",
+      appId: "1:883592815570:web:0a9f150c9ebee72197de0b"
+    }
+  };
+  
